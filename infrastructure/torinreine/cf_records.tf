b223bf9c4e82ea3848c67dfa37bd9a02da71f430
Tom Carrio <tom@carrio.dev>
2024-10-01 18:05:50 -0400

    chore: cleanup record names and remove redirect rule from before square site

--- infrastructure/torinreine/cf_records.tf
+++ infrastructure/torinreine/cf_records.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_record" "terraform_managed_resource_87bb7134421c5b51839083451761fd7c" {
+resource "cloudflare_record" "square_a_record" {
   comment = "square"
   content = "199.34.228.175"
   name    = "torinreine.com"
@@ -8,7 +8,7 @@ resource "cloudflare_record" "terraform_managed_resource_87bb7134421c5b518390834
   zone_id = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_028636cc125f4691984966439c11a5a3" {
+resource "cloudflare_record" "www_root_cname" {
   content = "torinreine.com"
   name    = "www"
   proxied = true
@@ -17,7 +17,7 @@ resource "cloudflare_record" "terraform_managed_resource_028636cc125f46919849664
   zone_id = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_5a0d1be55f277aca3cec6827413c154b" {
+resource "cloudflare_record" "zoho_mail_mx3" {
   content  = "mx3.zoho.com"
   name     = "torinreine.com"
   priority = 50
@@ -27,7 +27,7 @@ resource "cloudflare_record" "terraform_managed_resource_5a0d1be55f277aca3cec682
   zone_id  = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_439bad8916c5810a3eb199e1b264282d" {
+resource "cloudflare_record" "zoho_mail_mx2" {
   content  = "mx2.zoho.com"
   name     = "torinreine.com"
   priority = 20
@@ -37,7 +37,7 @@ resource "cloudflare_record" "terraform_managed_resource_439bad8916c5810a3eb199e
   zone_id  = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_944d8cb15dccc440b8cc56b03245592d" {
+resource "cloudflare_record" "zoho_mail_mx1" {
   content  = "mx.zoho.com"
   name     = "torinreine.com"
   priority = 10
@@ -47,7 +47,7 @@ resource "cloudflare_record" "terraform_managed_resource_944d8cb15dccc440b8cc56b
   zone_id  = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_7f347e5ded539f4bb87a64328b55faf1" {
+resource "cloudflare_record" "google_search_console_verification" {
   comment = "Google Search Console Verification"
   content = "google-site-verification=vRyOC0rRQcXCPMuT_DZsTrtdwlNW_RKYJxDJ6QFGM6A"
   name    = "torinreine.com"
@@ -57,7 +57,7 @@ resource "cloudflare_record" "terraform_managed_resource_7f347e5ded539f4bb87a643
   zone_id = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_143f8d9c6c892dd516352ff3c37ef541" {
+resource "cloudflare_record" "zoho_mail_spf" {
   content = "v=spf1 include:zohomail.com ~all"
   name    = "torinreine.com"
   proxied = false
@@ -67,6 +67,7 @@ resource "cloudflare_record" "terraform_managed_resource_143f8d9c6c892dd516352ff
 }
 
 resource "cloudflare_record" "terraform_managed_resource_9beb9878f32992fe54e688c5bc430cc5" {
+  comment = "Unknown verification record?"
   content = "8a063340-47d4-11ef-a188-4ba34efaaf07"
   name    = "torinreine.com"
   proxied = false
@@ -75,7 +76,7 @@ resource "cloudflare_record" "terraform_managed_resource_9beb9878f32992fe54e688c
   zone_id = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_dcec87ac468048b2845323c1ddc83380" {
+resource "cloudflare_record" "zoho_mail_verification" {
   comment = "Zoho Mail verification"
   content = "zoho-verification=zb34558348.zmverify.zoho.com"
   name    = "torinreine.com"
@@ -85,7 +86,7 @@ resource "cloudflare_record" "terraform_managed_resource_dcec87ac468048b2845323c
   zone_id = data.cloudflare_zone.torin_reine.id
 }
 
-resource "cloudflare_record" "terraform_managed_resource_adf13b8c6835e25cc4d299efdeeef817" {
+resource "cloudflare_record" "zoho_mail_dkim" {
   content = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCUUWvK8DLhwclI6GOb5Aix0kAHZF1/qUUl5Q2o3ob0Oym/NpDmGK71RepnAkKPy9uQKUVKEk8Gdi7NkaDtvWKt0HM305SiimNfwUHBwLZl5lr5tDuNrdTSZFptR9LXmFiwz8oFZ0hWPv5lcElOnhAsDWk4uBG4+clJhO/bSEu9TwIDAQAB"
   name    = "zmail._domainkey"
   proxied = false
